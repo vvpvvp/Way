40fea3bb5fc8ec026b3118b843d50e2478d54673
vvpvvp <alias8516@sina.com.cn>
2016-04-07 13:01:23 +0800

    暂时结构

---
+++ index.js
@@ -0,0 +1,4 @@
+import template from "./js/template";
+console.log(template.userInfo);
+console.log(template.userInfo2);
+console.log(1);
---
+++ js/template.js
@@ -0,0 +1,10 @@
+export default {
+	"userInfo":
+	`<div>
+		<div>{{a}}</div>
+	</div>`,
+	"userInfo2":
+	`<div>
+		<div>2</div>
+	</div>`
+}
